7bdb1de2d11cc7b9a3902d6316f0539727a3f4c9
Ingo Heimbach <i.heimbach@fz-juelich.de>
2018-01-11 11:13:05 +0100

    Added support if `issw` to the `xkb_layoyt` segment

--- segments/xkb_layout.sh
+++ segments/xkb_layout.sh
@@ -7,26 +7,44 @@
 # $ setxkbmod -query -v | awk -F "+" '{print $2}'
 # this will only work if you have set up XKB with a single layout which is true
 # for some.
+# On macOS `issw` is used instead (https://github.com/vovkasm/input-source-switcher)
 
 # This script will print the correct layout even if layout is set per window.
-# Exit if platform is not linux as this script is dependant on X11
+# Exit if platform is not linux or macOS as this script is dependant on X11
 
 run_segment() {
-	if ! shell_is_linux; then
+	if ! shell_is_linux && ! shell_is_osx; then
 		return 1
 	fi
 
-	cd "$TMUX_POWERLINE_DIR_SEGMENTS"
-	if [ ! -x "xkb_layout" ]; then
-		make clean xkb_layout &>/dev/null
-	fi
+	if shell_is_linux; then
+		cd "$TMUX_POWERLINE_DIR_SEGMENTS"
+		if [ ! -x "xkb_layout" ]; then
+			make clean xkb_layout &>/dev/null
+		fi
 
-	if [ -x ./xkb_layout ]; then
+		if [ ! -x ./xkb_layout ]; then
+			return 1
+		fi
 		cur_layout_nbr=$(($(./xkb_layout)+1));
 		cur_layout=$(setxkbmap -query | grep layout | sed 's/layout:\s\+//g' | \
 			awk -F ',' '{print $'$(echo "$cur_layout_nbr")'}')
-		echo "⌨  $cur_layout"
 	else
-		return 1
+		if ! which issw >/dev/null 2>&1; then
+			return 1
+		fi
+		cur_layout="$(issw | awk -F'.' '{ print tolower($NF) }')"
+		case $cur_layout in
+			uswithumlauts)
+				cur_layout="US"
+				;;
+			german)
+				cur_layout="DE"
+				;;
+			*)
+				;;
+		esac
 	fi
+
+	echo "⌨  $cur_layout"
 }
